52a3d88d9c2de7550d4912697eab903909a4cff9
Hemanth Gutlapalli <hemanth.gutlapalli@getsurance.de>
2023-08-18 18:39:58 +0200

    Initial commit

---
+++ models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,22 @@
+{{
+ config(
+ materialized = 'view'
+ )
+}}
+WITH src_hosts AS (
+    SELECT *
+    FROM
+        {{ ref('src_hosts') }}
+)
+
+SELECT
+    host_id,
+    is_superhost,
+    created_at,
+    updated_at,
+    COALESCE(
+        host_name,
+        'Anonymous'
+    ) AS host_name
+FROM
+    src_hosts
